27a2309a80d5a01c33440cc80715f0359a67c6b4
aggie-ch <aggie-ch@github.com>
2020-10-04 11:18:21 +0200

    Modified Main.class and renamed method in SumJoinStrategy on doJoin

--- src/main/java/io/github/aggie/Main.java
+++ src/main/java/io/github/aggie/Main.java
@@ -1,19 +1,13 @@
 package io.github.aggie;
 
-import io.github.aggie.cfg.ConfigProperties;
-import io.github.aggie.data.source.DataSource;
-import io.github.aggie.data.source.ApiDataSource;
-import io.github.aggie.data.source.RandomDataSource;
-import io.github.aggie.join.strategy.JoinStrategy;
-import io.github.aggie.join.strategy.SumJoinStrategy;
+import io.github.aggie.join.facade.JoinDataFacade;
+import io.github.aggie.join.facade.JoinDataFacadeFactory;
 
 public class Main {
     public static void main(String[] args) {
-        ConfigProperties configProperties = new ConfigProperties();
-        JoinStrategy<Integer> joinStrategy = new SumJoinStrategy();
-        DataSource<Integer> dataSource1 = new RandomDataSource();
-        DataSource<Integer> dataSource2 = new ApiDataSource(configProperties);
-        Integer result = joinStrategy.dataOperation(dataSource1, dataSource2);
+        JoinDataFacadeFactory joinDataFacadeFactory = new JoinDataFacadeFactory();
+        JoinDataFacade<Integer> joinDataFacade = joinDataFacadeFactory.createSumTwoRandomIntegersJoin();
+        Integer result = joinDataFacade.doJoin();
         System.out.println(result);
     }
 }
--- src/main/java/io/github/aggie/join/strategy/SumJoinStrategy.java
+++ src/main/java/io/github/aggie/join/strategy/SumJoinStrategy.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 
 public class SumJoinStrategy implements JoinStrategy<Integer> {
     @Override
-    public Integer dataOperation(DataSource<Integer> data1, DataSource<Integer> data2) {
+    public Integer doJoin(DataSource<Integer> data1, DataSource<Integer> data2) {
         Optional<Integer> number1 = Optional.ofNullable(data1.dataGenerate());
         Optional<Integer> number2 = Optional.ofNullable(data2.dataGenerate());
         Integer result = null;
